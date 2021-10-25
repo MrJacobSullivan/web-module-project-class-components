ba50d5d60f9819b9852c9aa82eaac840063849d1
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-25 14:38:02 -0500

    added input state to TodoForm and handle change and submit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,27 +1,35 @@
 import React from 'react'
 
 class TodoForm extends React.Component {
+  constructor() {
+    super()
+    this.state = { task: '' }
+  }
+
+  handleChange = (event) => {
+    this.setState({ ...this.state, task: event.target.value })
+  }
+
   handleSubmit = (event) => {
     event.preventDefault()
 
     // create new todo
     const newTodo = {
-      task: null,
+      task: this.state.task,
       id: Date.now(),
       completed: false,
     }
 
     this.props.handleAddItem(newTodo)
+    this.setState({ ...this.state, task: '' })
   }
 
-  handleClear = () => {
-    this.props.handleClearItems()
-  }
+  handleClear = () => this.props.handleClearItems()
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type='text' />
+        <input type='text' name='task' value={this.state.task} onChange={this.handleChange} />
         <button>Add Todo</button>
         <button type='button' onClick={this.handleClear}>
           Clear Completed
