68139a9cecdd5b0ab9c99ec79bdc549023c0353c
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-25 13:30:22 -0500

    added TodoForm DOM structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
 
 class App extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList todos={[{ name: 'Dishes', id: 1 }]} />
+        <TodoForm />
       </div>
     )
   }
